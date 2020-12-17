c1edb01998f0c5bb411883c35c3b02eb92cf029f
JordanBoaventura <j-boaventura@outlook.com>
2020-12-16 21:29:43 -0300

    mudando setting para deubug false

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(&$%@9#kz0d@jr#yabe@#1%3mp+vh=bmr@#^noilnt&$a+vs-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
