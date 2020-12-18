c815545e609d0de9792976d0b17f0b4c33b1e0ca
JordanBoaventura <j-boaventura@outlook.com>
2020-12-18 01:24:02 -0300

    mudando para base heroku

--- blog/settings.py
+++ blog/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -93,13 +93,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-
 """
+
+
 DATABASES = {
     'default': dj_database_url.config()
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
