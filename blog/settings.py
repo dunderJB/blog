36372d20b8925674d6afce1bedfe4b151efac1aa
JordanBoaventura <j-boaventura@outlook.com>
2020-12-20 22:17:55 -0300

    ajustando para producao

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "(&$%@9#kz0d@jr#yabe@#1%3mp+vh=bmr@#^noilnt&$a+vs-3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -84,23 +84,20 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "blog",
-        "USER": "postgres",
-        "PASSWORD": "123456789",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "blog",
+#         "USER": "postgres",
+#         "PASSWORD": "123456789",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 
-"""
-DATABASES = {
-    'default': dj_database_url.config()
-}
-"""
+DATABASES = {"default": dj_database_url.config()}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
