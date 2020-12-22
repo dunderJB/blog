20e8956dff567ddfa00a0993925feea6b1f5d335
JordanBoaventura <j-boaventura@outlook.com>
2020-12-21 21:31:36 -0300

    teste

--- blog/settings.py
+++ blog/settings.py
@@ -161,19 +161,3 @@ AUTHENTICATION_BACKENDS = [
     # Django ModelBackend is the default authentication backend.
     "django.contrib.auth.backends.ModelBackend",
 ]
-
-
-AWS_ACCESS_KEY_ID = "AKIASU7TRVYCXY3RNSOK"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "jb-blog-django"
-AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    "CacheControl": "max-age=86400",
-}
-AWS_LOCATION = "static"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "blog/static"),
-]
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
